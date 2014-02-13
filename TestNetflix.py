ae21f39cd79578ca96772999f150701ead779aaf
Paul Strong <lapluviosilla@gmail.com>
2014-02-13 01:43:07 -0600

    Added initial test cases for rmse. References #3 and #10

---
+++ TestNetflix.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+To test the program:
+    % python TestNetflix.py > TestNetflix.out
+    % chmod ugo+x TestNetflix.py
+    % TestNetflix.py > TestNetflix.out
+"""
+
+import io
+import unittest
+
+from Netflix import netflix_solve, rmse
+
+class TestNetflix (unittest.TestCase) :
+  def test_rmse1 (self) :
+    a = (2,3,4)
+    b = (2,3,4)
+    self.assertTrue(str(rmse(a,b)) == "0.0")
+  
+  def test_rmse2 (self) :
+    a = [2,3,4]
+    b = (3,4,5)
+    self.assertTrue(str(rmse(a,b)) == "1.0")
+    self.assertTrue(str(rmse(b,a)) == "1.0")
+
+  def test_rmse3 (self) :
+    a = (1,4,5,8,9)
+    b = (1,5,5,4,8)
+    self.assertTrue(str(round(rmse(a,b), 6)) == "1.897367")
+
+print("TestNetflix.py")
+unittest.main()
+print("Done.")
