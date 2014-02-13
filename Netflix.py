b26bad9a8fa03f61eed03644a43ff9f996b8f19c
Paul Strong <lapluviosilla@gmail.com>
2014-02-13 05:24:23 -0600

    Now calculating RMSE for basic predictor. Closes #10 and closes #11

--- Netflix.py
+++ Netflix.py
@@ -9,22 +9,61 @@ BASE_CACHE_PATH = "../netflix-tests/"
 CACHE_PROBE = 0
 CACHE_GENERAL = 1
 
+cache_probe = {}
+
+class Prediction:
+  def __init__(self, movie, custids, predictions):
+    self.movie = movie
+    self.custids = custids
+    self.predictions = predictions
+
+  def display(self, writer):
+    writer.write(str(self.movie) + ":\n")
+    for rating in self.predictions:
+      writer.write(str(round(rating, 1)) + "\n") 
+
+  def calculate_rmse(self) :
+    return rmse(self.predictions, map(lambda x: get_cache_probe()[self.movie][x], self.custids))
+
+
 def netflix_solve(r, w) :
   """
   read, eval, print loop
   r is a reader
   w is a writer
   """
+  get_cache_probe() # Load probe cache if it has not been loaded yet
+
+  assert(len(cache_probe) > 0)
+
+  predictions = []
+
   movie = 0
+  custids = []
   for line in r:
     if line.strip().endswith(":"):
+      if len(custids) > 0:
+        # at end of movie
+        predictions.append(netflix_predict(movie, custids))
+        custids = []
       line = line.strip().rstrip(":")
       movie = int(line)
-      w.write(line + ":\n")
+      # w.write(line + ":\n")
     else:
       custid = int(line)
-      w.write(str(3.7) + "\n")
+      custids.append(custid)
+      # w.write(str(3.7) + "\n")
+  if len(custids) > 0 : predictions.append(netflix_predict(movie, custids))
 
+  assert(len(predictions) > 0)
+  netflix_print(w, predictions)
+
+def netflix_predict(movie, custids) :
+  assert(movie > 0)
+  assert(len(custids) > 0)
+  prediction = Prediction(movie, custids, len(custids) * [3.7])
+  assert(prediction is not None)
+  return prediction
 
   # with open("irvin-user_avg_json", "r")
 
@@ -56,12 +95,27 @@ def netflix_load_cache(cache_type, path):
 
   return cache
 
+def netflix_print(w, predictions) :
+  for prediction in predictions :
+    prediction.display(w)
 
+  v = sum((prediction.calculate_rmse() ** 2 for prediction in predictions), 0.0)
+  final_rmse = math.sqrt(v / len(predictions))
+
+  w.write("RMSE: " + str(final_rmse) + "\n")
 
 def rmse(a, b) :
   """
   Calculate Root Mean Squared Error between two iterables a and b
   """
   z = zip(a, b)
-  v = sum(((x - y) ** 2 for x, y in z), 0.0)
+  v = sum(((float(x) - float(y)) ** 2 for x, y in z), 0.0)
   return math.sqrt(v / len(a))
+
+def get_cache_probe():
+  global cache_probe
+  cache_probe = cache_probe or netflix_load_cache(CACHE_PROBE, "word-probeMapCache.txt")
+  return cache_probe
+def set_cache_probe(val):
+  global cache_probe
+  cache_probe = val
--- TestNetflix.py
+++ TestNetflix.py
@@ -10,7 +10,7 @@ To test the program:
 import io
 import unittest
 
-from Netflix import netflix_solve, rmse, netflix_load_cache, CACHE_PROBE, CACHE_GENERAL
+from Netflix import netflix_solve, netflix_predict, rmse, netflix_load_cache, set_cache_probe, get_cache_probe, Prediction, CACHE_PROBE, CACHE_GENERAL
 
 class TestNetflix (unittest.TestCase) :
   # ---
@@ -20,19 +20,65 @@ class TestNetflix (unittest.TestCase) :
     r = io.StringIO("1:\n14756\n")
     w = io.StringIO()
     netflix_solve(r, w)
-    self.assertTrue(w.getvalue() == "1:\n3.7\n")
+    self.assertEqual(w.getvalue(), "1:\n3.7\nRMSE: 0.2999999999999998\n")
 
   def test_solve2 (self) :
     r = io.StringIO("1:\n712610\n1772839\n1059319\n2380848\n548064\n")
     w = io.StringIO()
     netflix_solve(r, w)
-    self.assertTrue(w.getvalue() == "1:\n3.7\n3.7\n3.7\n3.7\n3.7\n")
+    self.assertEqual(w.getvalue(), "1:\n3.7\n3.7\n3.7\n3.7\n3.7\nRMSE: 1.0630145812734648\n")
 
   def test_solve3 (self) :
     r = io.StringIO("10007:\n1204847\n2160216\n248206\n")
     w = io.StringIO()
     netflix_solve(r, w)
-    self.assertTrue(w.getvalue() == "10007:\n3.7\n3.7\n3.7\n")
+    self.assertEqual(w.getvalue(), "10007:\n3.7\n3.7\n3.7\nRMSE: 1.7767010628315238\n")
+
+  # ---
+  # netflix_predict
+  # ---
+  def test_predict(self) :
+    prediction = netflix_predict(1, (4,8,10,12))
+    self.assertEqual(prediction.movie, 1)
+    self.assertEqual(prediction.predictions, 4 * [3.7])
+
+  def test_predict2(self) :
+    prediction = netflix_predict(2, (1,2,3,4))
+    s = io.StringIO()
+    self.assertEqual(prediction.movie, 2)
+    prediction.display(s)
+    self.assertEqual(s.getvalue(), "2:\n3.7\n3.7\n3.7\n3.7\n")
+
+  def test_predict3(self) :
+    prediction = netflix_predict(1, (1,2,3))
+    self.assertTrue(type(prediction), Prediction)
+
+  def test_prediction_display(self) :
+    prediction = Prediction(3, [5,6,7], [3.42, 4.21, 2.18])
+    s = io.StringIO()
+    prediction.display(s)
+    self.assertEqual(s.getvalue(), "3:\n3.4\n4.2\n2.2\n")
+
+  def test_prediction_display2(self) :
+    prediction = Prediction(2, [1,2], [2,3])
+    s = io.StringIO()
+    prediction.display(s)
+    self.assertEqual(s.getvalue(), "2:\n2\n3\n")
+
+  def test_prediction_rmse(self) :
+    set_cache_probe({1: {1: 3, 4:2, 5:5}})
+    prediction = Prediction(1, [1,4,5], [3.7, 2.1, 4.2])
+    r = prediction.calculate_rmse()
+    set_cache_probe(None)
+    self.assertEqual(r, 0.6164414002968976)
+
+  def test_prediction_rmse2(self) :
+    set_cache_probe({1: {1: 3, 4:2, 5:5}})
+    prediction = Prediction(1, [1,4,5], [3, 2, 5])
+    r = prediction.calculate_rmse()
+    set_cache_probe(None)
+    self.assertEqual(r, 0.0)
+
 
   # ---
   # netflix_load_cache
@@ -54,6 +100,11 @@ class TestNetflix (unittest.TestCase) :
     self.assertEqual(len(cache), 17770)
     self.assertEqual(cache[4863], 3.0)
     self.assertEqual(cache[5267], 3.395734597156398)
+
+  def test_get_cache_probe(self) :
+    cache = get_cache_probe()
+    self.assertEqual(len(cache[10]), 2)
+    self.assertEqual(cache[10][1531863], 3)
 
   # ---
   # rmse
